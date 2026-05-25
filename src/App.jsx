00a3a6e42a3c652b70b0b75dc91a89d8645fb0e3
fury <antoniosteven664@gmail.com>
2026-05-26 00:51:21 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Swal from 'sweetalert2'
-import Login from './components/login'
+import Login from './components/Login'
 import ForgotPassword from './components/ForgotPassword'
 
 function App() {
